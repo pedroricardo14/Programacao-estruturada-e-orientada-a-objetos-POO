f07971981ca9f1d4ea90cc5ca21be9ca31d1bb58
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-20 14:33:13 -0300

    Create classePessoa.py

---
+++ classePessoa.py
@@ -0,0 +1,24 @@
+class Pessoa:
+    __nome=None
+    __idade=None
+    
+    def __init__(self, nome, idade):
+        self.__nome = nome
+        self.__idade = idade
+        
+    def Pessoa():
+        pass
+
+    def setNome(self, nome):
+        self.__nome = nome
+
+    def getNome(self):
+        return  self.__nome
+
+    def setIdade(self, idade):
+        self.__idade = idade
+
+    def getIdade(self):
+        return self.__idade
+
+    # pode importa os arquivos de classes
