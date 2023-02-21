90cd10b4f71bb54591735153b735df9faae5c43b
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-21 09:32:46 -0300

    Create heranca_q1.py

---
+++ atividades_em_classe/heranca_q1.py
@@ -0,0 +1,22 @@
+class Ingresso:
+    valor = 0.0
+    def info(self):
+        print("Valor = R$", self.valor)
+        
+        
+class IngressoVIP(Ingresso):
+    valorAdicional = 0.0
+    def info(self):
+        print("Valor VIP = R$", self.valor+self.valorAdicional)
+    
+## TESTE INGRESSOVIP ##
+vip1 = IngressoVIP()
+vip1.valor = 30.00
+vip1.valorAdicional = 15.00
+vip1.info()
+
+
+'''# TESTE INGRESSO ##
+ingresso1 = Ingresso()
+ingresso1.valor = 30.0
+ingresso1.info()'''
