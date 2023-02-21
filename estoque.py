0e452655b89b702eab2e2b507efeda66d77fb9b6
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-21 09:23:24 -0300

    Create estoque.py

---
+++ estoque.py
@@ -0,0 +1,22 @@
+class Produto:
+    def __init__(self, nome_t)
+        self.nome = nome_t    
+        self.estoque = 0
+    def info(self):
+        print(f"{self.nome} / {self.__estoque}")
+        
+    def incluir(self, qtd):
+        self.__estoque += qtd
+        
+    def vender(self, qtd):
+        self.__estoque -= qtd
+        
+##########################################################
+        
+p1 = Produto("Nescau")
+p1.incluir(5)
+p1.info()
+p1.inclur(2)
+p1.info
+p1.vender(6)
+p1.info
