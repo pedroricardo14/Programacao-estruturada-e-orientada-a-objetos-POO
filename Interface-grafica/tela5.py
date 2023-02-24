505e9e3f7f5933313726a2675618d66941877156
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-02-24 19:52:04 -0300

    Create tela5.py

---
+++ Interface-grafica/tela5.py
@@ -0,0 +1,20 @@
+from tkinter import *
+janela = Tk()
+rot1 = Label(janela,text="Valor 1:").grid(row=0,column=0)
+campo1 = Entry(janela).grid(row=0, column=1)
+rot2 = Label(janela,text="Valor 2:")
+.grid(row=1,column=1)
+campo2 = Entry(janela).grid(row=1, column=1)
+rot3 = Label(janela, text="Total =")
+.grid(row=3, column=3)
+campo3 = Entry(janela).grid(row=3, column=1)
+def somar():
+  v1 = int(campo1.get())
+  v2 = int(campo2.get())
+  total = v1 + v2
+  campo3.delete(0, END)
+  campo3.insert(0, total)
+
+bot = Button(janela, text="Somar", command=somar).grid(row=3, column=1)
+
+janela.mainloop()
